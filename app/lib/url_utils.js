34fb69d4506103de2df3b280dfae030239485077
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2018-01-05 15:34:15 +1100

    Add security headers to NGinx + remote Dailymotion / SoundCloud embed

--- app/lib/url_utils.js
+++ app/lib/url_utils.js
@@ -5,9 +5,3 @@ export const optionsToQueryString = (options) => (
 
 export const youtubeRegex =
   /^(http(s)?:\/\/)?(www\.)?((youtube\.com\/watch\?v=)|(youtu.be\/))([a-zA-Z0-9\-_])+/
-
-export const dailymotionRegex =
-  /^.+dailymotion.com\/(video|hub)\/([^_]+)[^#]*(#video=([^_&]+))?/
-
-export const soundcloudRegex =
-  /^https?:\/\/(soundcloud.com|snd.sc)\/([a-z0-9-_]+\/[a-z0-9-_]+)$/
--- config/prod/env.sh
+++ config/prod/env.sh
@@ -4,4 +4,5 @@ JS_ENV='prod'
 HTTP_API_URL='https://api.captainfact.io'
 WS_API_URL='wss://api.captainfact.io/socket'
 FRONTEND_URL='https://captainfact.io'
+STATIC_RESOURCES_URL="https://s3.amazonaws.com/captainfact/resources/" # Keep trailing /
 FB_APP_ID='1502183533135523'
