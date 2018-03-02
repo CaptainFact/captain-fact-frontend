f7a2d89a1445dcd4ddb610d8468b5c55730bc39d
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2018-03-02 18:41:27 +1100

    Add description and styles to browser extension page

---
+++ app/i18n/fr/extension.js
@@ -0,0 +1,5 @@
+export default {
+  "description": "L'extension pour navigateur permet d'incruster les informations fournies sur CaptainFact directement sur le contenu que vous regardez.",
+  "moreInfo": "Elle est 100% <1>open-source</1> et <3>respecte votre vie privée</3>.",
+  "addTo": "Ajouter à"
+}
