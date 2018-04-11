f927ea0c8965cbc53b3bd6fd6ab12a4413abf8a7
NGambini <noe.gambini@gmail.com>
2018-04-11 20:38:52 +0200

    [~] Fixed steps selector

--- app/state/onboarding_steps/selectors.js
+++ app/state/onboarding_steps/selectors.js
@@ -1,3 +1,2 @@
 export const uncompletedOnboardingSteps = state => 
-  state.OnboardingSteps.steps
-  // state.OnboardingSteps.steps.filter(s => !state.CurrentUser.data.onboarding_completed.includes(s.uniqueId))
+  state.OnboardingSteps.steps.filter(s => !state.CurrentUser.data.onboarding_completed.includes(s.uniqueId))
