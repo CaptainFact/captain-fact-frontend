00238e92005b48b3b76c14c86bb8b120262502e1
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2018-05-02 19:20:23 +0200

    Fix CI by removing ReputationGuard spec

--- app/components/Utils/ErrorView.jsx
+++ app/components/Utils/ErrorView.jsx
@@ -1,8 +1,8 @@
-import React from "react"
-import { Link, withRouter } from "react-router"
-
-import { LinkWithIcon } from "../Utils"
+import React from 'react'
+import { Link, withRouter } from 'react-router'
 import { translate } from 'react-i18next'
+
+import { LinkWithIcon } from '../Utils'
 import Message from './Message'
 import { getErrorInfo, tError } from '../../lib/errors'
 
@@ -13,6 +13,34 @@ const refreshableErrors = ['join_crashed']
 @withRouter
 @translate('errors')
 export class ErrorView extends React.PureComponent {
+  render() {
+    const { t, error='unknown', canGoBack=true } = this.props
+    const canReload = this.props.canReload || refreshableErrors.includes(error)
+    return (
+      <div className="message-view">
+        <Message
+          type="danger"
+          header={<p><strong>{t('title')}</strong></p>}>
+          <div>
+            <p>{tError(t, error)}{this.getMoreInfo()}</p>
+            {(canGoBack || canReload) && <br/>}
+            {canGoBack &&
+                <LinkWithIcon iconName="arrow-left"
+                  onClick={() => this.props.router.goBack()}>
+                  {t('main:actions.goBack')}
+                </LinkWithIcon>}
+            {canReload &&
+                <LinkWithIcon iconName="refresh"
+                  onClick={() => location.reload()}
+                  style={{float: 'right'}}>
+                  {t('main:actions.reload')}
+                </LinkWithIcon>}
+          </div>
+        </Message>
+      </div>
+    )
+  }
+
   getMoreInfo() {
     const errorInfo = getErrorInfo(this.props.error)
     if (!errorInfo)
@@ -23,34 +51,6 @@ export class ErrorView extends React.PureComponent {
           {this.props.t(errorInfo.i18nKey || 'main:actions.moreInfo')}
         </Link>
       </span>
-    )
-  }
-
-  render() {
-    const { t, error='unknown', canGoBack=true } = this.props
-    const canReload = this.props.canReload || refreshableErrors.includes(error)
-    return (
-      <div className="message-view">
-        <Message
-          type="danger"
-          header={<p><strong>{t('title')}</strong></p>}>
-            <div>
-              <p>{tError(t, error)}{this.getMoreInfo()}</p>
-              {(canGoBack || canReload) && <br/>}
-              {canGoBack &&
-                <LinkWithIcon iconName="arrow-left"
-                              onClick={() => this.props.router.goBack()}>
-                  {t('main:actions.goBack')}
-                </LinkWithIcon>}
-              {canReload &&
-                <LinkWithIcon iconName="refresh"
-                              onClick={() => location.reload()}
-                              style={{float: 'right'}}>
-                  {t('main:actions.reload')}
-                </LinkWithIcon>}
-            </div>
-        </Message>
-      </div>
     )
   }
 }
