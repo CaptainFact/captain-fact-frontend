2c8344ba43ff121b4b1f197868b95e605a5e4e74
Emmanuel Haguet <emmanuel.haguet@laposte.net>
2018-07-02 17:27:03 +0200

    Fix translation in history tab
    fix "Unnecessarily quoted property"

--- app/i18n/en/keys.js
+++ app/i18n/en/keys.js
@@ -1,5 +1,5 @@
 export default {
-  'url': 'Url in keys.js (en)',
-  'statements_time': 'Shift all statements (in seconds)',
-  'full_name': 'Full name of speaker'
+  url: 'Url',
+  statements_time: 'Shift all statements (in seconds)',
+  full_name: 'Full name of speaker'
 }
--- app/i18n/fr/keys.js
+++ app/i18n/fr/keys.js
@@ -1,5 +1,5 @@
 export default {
-  'url': 'Url in keys.js (fr)',
-  'statements_time': 'Décalage de l\'ensemble des affirmations (en secondes)',
-  "full_name": 'Nom complet de l\'intervenant'
+  url: 'Url',
+  statements_time: 'Décalage de l\'ensemble des affirmations (en secondes)',
+  full_name: 'Nom complet de l\'intervenant'
 }
