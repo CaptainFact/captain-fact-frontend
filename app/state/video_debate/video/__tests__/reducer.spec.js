a5dbde10cbe32ffb6015373280925836fa0d44b0
Facundo Rossi <Rossifacundo94@gmail.com>
2018-10-07 16:42:48 -0300

    Add UT for video-debate/video/reducer (#221)
    * Add UT for video-debate/video/reducer
    * Mock the uuid module

---
+++ app/state/video_debate/video/__tests__/reducer.spec.js
@@ -0,0 +1,65 @@
+import reducer, {
+    fetchAll,
+    setLoading,
+    addSpeaker,
+    removeSpeaker,
+    updateSpeaker,
+    setPosition,
+    forcePosition
+
+} from '../reducer'
+import fetchAllSuccess from './__fixtures__/fetch_all_success.json'
+
+const INITIAL_STATE = reducer(undefined, {})
+
+test('has correct defaults', () => {
+    snapshot(INITIAL_STATE)
+})
+
+test('fetch all', () => {
+    snapshotReducer(reducer, INITIAL_STATE, fetchAll(fetchAllSuccess))
+})
+
+test('set Loading', () => {
+    snapshotReducer(reducer, INITIAL_STATE, setLoading(true), setLoading(false))
+})
+
+test('add speaker', () => {
+    snapshotReducer(reducer, INITIAL_STATE, addSpeaker(
+        {
+            id: 3,
+            title: "Title 3",
+            full_name: "Max Fray"
+        }
+    ))
+})
+
+test('remove speaker', () => {
+    snapshotReducer(reducer, INITIAL_STATE, removeSpeaker(3))
+})
+
+test('update speaker', () => {
+    snapshotReducer(reducer, INITIAL_STATE, updateSpeaker(
+        {
+            id: 3,
+            title: "Title 3 Update",
+            full_name: "Max Fray Update"
+        }
+    ))
+})
+
+test('set Position', () => {
+    snapshotReducer(reducer, INITIAL_STATE, setPosition({ position: 4 }))
+})
+
+test('force Position', () => {
+    snapshotReducer(reducer, INITIAL_STATE, forcePosition(
+        {
+            position: 4,
+            forcedPosition: {
+                requestId: 1,
+                time: 0,
+            }
+        }
+    ))
+})
