83924fef94cc0f6fe7402009a0a356c86441621e
Naomi Hauret <naomi@troopers.email>
2018-10-20 16:20:00 +0200

    enhanced webpack production workflow

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -3,18 +3,22 @@ const path = require('path')
 
 // Plugins
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const AsyncChunkNamesPlugin = require("webpack-async-chunk-names-plugin")
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const CompressionPlugin = require('compression-webpack-plugin')
+const CompressionPlugin = require('brotli-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const LodashModuleReplacementPlugin = require('lodash-webpack-plugin')
+const ImageminPlugin = require('imagemin-webpack-plugin').default
 const Dotenv = require('dotenv-webpack')
 const loadersConf = require('./webpack.loaders')
 
 module.exports = {
   mode: 'production',
+  devtool: false,
   entry: {
     app: [
       'babel-polyfill',
@@ -39,21 +43,60 @@ module.exports = {
   optimization: {
     splitChunks: {
       cacheGroups: {
-        commons: {
-          test: /[\\/]node_modules[\\/]/,
+        default: false,
+        vendors: false,
+        vendor: {
           name: 'vendor',
-          chunks: 'all'
+          chunks: 'all',
+          test: /node_modules/,
+          priority: 20
+        },
+        common: {
+          name: 'common',
+          minChunks: 2,
+          chunks: 'all',
+          priority: 10,
+          reuseExistingChunk: true,
+          enforce: true
         }
       }
     },
     minimizer: [
       new UglifyJsPlugin({
-        cache: true,
-        parallel: true,
-        sourceMap: true
+        uglifyOptions: {
+          ecma: 8,
+          warnings: false,
+          compress: {
+            warnings: false,
+            conditionals: true,
+            unused: true,
+            comparisons: true,
+            sequences: true,
+            dead_code: true,
+            evaluate: true,
+            if_return: true,
+            join_vars: true,
+            drop_console: true,
+            drop_debugger: true,
+          },
+          output: {
+            comments: false,
+            beautify: false,
+          },
+          sourceMap: false,
+          pure_funcs: ['console.log'],
+          toplevel: false,
+          nameCache: null,
+          ie8: false,
+          keep_classnames: undefined,
+          keep_fnames: false,
+          safari10: false,
+        },
       }),
-      new OptimizeCSSAssetsPlugin({})
-    ]
+      new OptimizeCSSAssetsPlugin({
+        cssProcessorOptions: { discardComments: { removeAll: true } },
+      }),
+    ],
   },
   module: {
     rules: loadersConf(true)
@@ -81,11 +124,25 @@ module.exports = {
     // load the bundles into an html template
     new HtmlWebpackPlugin({
       template: 'app/index.html',
-      minify: true
+      minify: {
+        collapseWhitespace: true,
+        collapseInlineTagWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+      },
     }),
+    new AsyncChunkNamesPlugin(),
+    new LodashModuleReplacementPlugin({
+      collections: true,
+      paths: true,
+    }),
+    new ImageminPlugin({ test: /\.(jpe?g|png|gif|svg)$/i }),
     // gzip
     new CompressionPlugin({
-      test: /\.(js|css|html)$/
+      algorithm: "gzip",
+      asset: "[path].gz[query]",
+      test: /\.(html|js|css|svg|ttf|eot|otf|woff|ico)$/,
+      minRatio: 0.5,
     }),
     // loads up .env file
     new Dotenv({
