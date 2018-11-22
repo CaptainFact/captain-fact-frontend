315af0fddc019e130a8973bf65d8d64df3c6ba1f
Trystan Eveno <trystan.eveno@lephare.com>
2018-11-22 23:24:28 +0100

    timeline is working great && now create auto focus quote with video position

--- app/components/VideoRefacted/Timeline.jsx
+++ app/components/VideoRefacted/Timeline.jsx
@@ -1,29 +1,63 @@
 import PropType from 'prop-types';
 import React, { PureComponent } from 'react';
 
-const Timeline = ({ facts, currentTime, totalTime }) => {
-  return (
-    <div className="wrapper-timeline">
-      <h3 className="mb-4 text-center">{ facts.length } facts checking</h3>
-      <div className="timeline"></div>
-      <div className="time">
-        <p>{ currentTime }</p>
-        <p className="ml-auto">{ totalTime }</p>
+import { formatHour } from "../../state/utils";
+
+class Timeline extends PureComponent {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidUpdate() {
+    const iconsQuotes = document.querySelectorAll('.icon-quote');
+
+    for (let i = 0; i < iconsQuotes.length; i++) {
+      console.log();
+      iconsQuotes[i].style.left = `${document.querySelector('.timeline').offsetWidth / this.props.totalTime * parseInt(iconsQuotes[i].getAttribute('data-time'))}px`;
+      iconsQuotes[i].style.zIndex = i + 1;
+    }
+
+    document.querySelector('.timeline-played').style.width = `${document.querySelector('.timeline').offsetWidth / this.props.totalTime * this.props.currentTime}px`;
+  }
+
+  render() {
+    const props = this.props;
+    let domIconsQuotes = [];
+
+    for (let i = 0; i < props.facts.length; i++) {
+      domIconsQuotes.push(
+        <div key={ `icon-quote-${ i }` } className="icon-quote" data-time={ props.facts[i].time }>
+          <img src={ props.facts[i].icon } alt=""/>
+        </div>
+      );
+    }
+
+    return (
+      <div className="wrapper-timeline">
+        <h3 className="mb-4 text-center">{ props.facts.length } facts checking</h3>
+        <div className="timeline">
+          <div className="timeline-played"></div>
+          { domIconsQuotes }
+        </div>
+        <div className="time mt-3">
+          <p>{ formatHour(props.currentTime) }</p>
+          <p className="ml-auto">{ formatHour(props.totalTime) }</p>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 Timeline.defaultProps = {
-  currentTime: '00:00:00',
-  facts: 0,
-  totalTime: '00:00:00'
+  currentTime: 0,
+  facts: [],
+  totalTime: 0
 };
 
 Timeline.propTypes = {
-  currentTime: PropType.string,
+  currentTime: PropType.number,
   facts: PropType.array,
-  totalTime: PropType.string
+  totalTime: PropType.number
 };
 
 export default Timeline;
--- app/state/utils.js
+++ app/state/utils.js
@@ -124,3 +124,23 @@ export function isAction(obj) {
 function isIterable(obj) {
   return obj !== null && typeof (obj[Symbol.iterator]) === 'function'
 }
+
+export const formatHour = currentTime => {
+  let sec_num = parseInt(currentTime, 10);
+  let hours   = Math.floor(sec_num / 3600);
+  let minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+  let seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+  if (hours < 10) {
+    hours = '0' + hours;
+  }
+  if (minutes < 10) {
+    minutes = '0' + minutes;
+  }
+  if (seconds < 10) {
+    seconds = '0' + seconds;
+  }
+
+  return hours + ':' + minutes + ':' + seconds;
+};
+
