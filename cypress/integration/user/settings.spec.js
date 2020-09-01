c841e14e88df2093d7c35fdba9e0009b77f38e08
Benjamin Piouffle <benjamin@opencollective.com>
2020-09-01 20:46:04 +0200

    test: Fix E2E

--- cypress/integration/user/settings.spec.js
+++ cypress/integration/user/settings.spec.js
@@ -2,37 +2,30 @@ import faker from 'faker'
 
 describe('Settings', () => {
   beforeEach(() => {
-    cy.login().then(user => {
+    cy.login().then((user) => {
       cy.visit(`/u/${user.username}/settings`)
     })
   })
 
   it('can change language', () => {
-    cy.get('.user-page .language-selector select')
-      .select('fr')
-      .should('have.value', 'fr')
+    cy.get('.user-page .language-selector select').select('fr').should('have.value', 'fr')
     cy.contains('Langue').should('exist')
   })
 
   it('can edit name', () => {
-    const newName = faker.name.findName()
+    const newName = faker.name.firstName()
     cy.get('input[name="name"]')
       .clear()
       .type(`${newName}{enter}`)
       .then(() => {
-        cy.get('.main-appelation').should('be', newName)
-        cy.get('input[name="name"]').should('be', newName)
+        cy.get('.main-appelation').should('contain', newName)
+        cy.get('input[name="name"]').should('have.value', newName)
       })
   })
 
   it("fails to change password if they doesn't match", () => {
-    cy.get('input[name="password"]')
-      .clear()
-      .type('password{enter}')
-    cy.get('input[name="passwordRepeat"]')
-      .clear()
-      .type('notTheSame{enter}')
-      .blur()
+    cy.get('input[name="password"]').clear().type('password{enter}')
+    cy.get('input[name="passwordRepeat"]').clear().type('notTheSame{enter}').blur()
     cy.contains("Passwords doesn't match").should('exist')
   })
 })
