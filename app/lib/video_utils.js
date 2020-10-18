dfd53856b6950051d5c11f81349fd261332461df
steelstyle <404554+steelstyle@users.noreply.github.com>
2020-10-18 14:32:27 +0200

    Uses HD thumbnail for opengraph when available.

--- app/lib/video_utils.js
+++ app/lib/video_utils.js
@@ -19,6 +19,17 @@ export const getTimecode = (videoPlayer, video, baseTimecode) => {
   return getTimecodesOffset(video, videoPlayer) + baseTimecode
 }
 
+/**
+ * Returns HD thumbnail URL if any, otherwise returns the default thumbnail URL.
+ */
+export const getHDThumbnailUrl = (video) => {
+  if (video.youtube_id) {
+    return `https://img.youtube.com/vi/${video.youtube_id}/hqdefault.jpg`
+  }
+
+  return video.thumbnail
+}
+
 export const THUMBNAILS_SIZES = {
   MEDIUM: 'MEDIUM',
   LARGE: 'LARGE',
