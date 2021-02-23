47fbfc58efaf76bd6cfe7d0d3beabe8761c6ff10
Wid-Gaël Destin <wid.destin@gmail.com>
2021-02-23 18:42:25 +0100

    Added iframe for statement's source

---
+++ app/components/Comments/ModalSource.jsx
@@ -0,0 +1,92 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import Modal from '../Modal/Modal'
+import { popModal } from '../../state/modals/reducer'
+
+import { LoadingFrame } from '../Utils/LoadingFrame'
+import ExternalLinkNewTab from '../Utils/ExternalLinkNewTab'
+
+import styled from 'styled-components'
+import { Flex, Box } from '@rebass/grid'
+import { TimesCircle } from 'styled-icons/fa-solid/TimesCircle'
+import { LinkExternal } from 'styled-icons/octicons/LinkExternal'
+
+const CloseButton = styled(({ size = '1em', ...props }) => (
+  <TimesCircle size={size} cursor="pointer" {...props} />
+))`
+  color: #d0d0d0;
+  &:hover {
+    opacity: 0.75;
+  }
+`
+
+const UrlBarHeader = styled(Flex)`
+  border-bottom: 1px solid #dbdbdb;
+  background-color: white;
+  padding: 10px;
+  position: relative;
+`
+const StyledExternalLinkNewTab = styled(ExternalLinkNewTab)`
+  color: black;
+`
+
+const UrlBarTitle = styled(Box)`
+  display: inline-block;
+  vertical-align: middle;
+  width: 300px;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+`
+
+const IframeContainer = styled.div`
+  background-color: white;
+  height: 100%;
+`
+
+class ModalSource extends React.PureComponent {
+  state = { isLoading: true }
+  renderUrlBarHeader = (url, title, website_name) => {
+    const { popModal } = this.props
+
+    return (
+      <UrlBarHeader justifyContent="space-between">
+        <Box>
+          <StyledExternalLinkNewTab href={url}>
+            <LinkExternal size="1em" />
+            <UrlBarTitle ml="0.2em">{title === null ? website_name : title}</UrlBarTitle>
+          </StyledExternalLinkNewTab>
+        </Box>
+        <CloseButton size="1.5em" title="Close" onClick={popModal} />
+      </UrlBarHeader>
+    )
+  }
+
+  render() {
+    const { source, ...otherProps } = this.props
+    const { url, title, website_name } = source
+    return (
+      <Modal
+        customHeader={this.renderUrlBarHeader(url, title, website_name)}
+        overrideContentStructure
+        isModalSource
+        {...otherProps}
+      >
+        <IframeContainer>
+          {this.state.isLoading && <LoadingFrame />}
+          <iframe
+            src={url}
+            style={{
+              width: '100%',
+              height: '100%',
+              display: this.state.isLoading ? 'none' : 'block',
+            }}
+            onLoad={() => this.setState({ isLoading: false })}
+          />
+        </IframeContainer>
+      </Modal>
+    )
+  }
+}
+
+export default connect(null, { popModal })(ModalSource)
--- app/components/Modal/Modal.jsx
+++ app/components/Modal/Modal.jsx
@@ -12,21 +12,24 @@ const Modal = ({
   popModal,
   isActive = true,
   title = null,
+  customHeader = null,
   children = null,
   footer = null,
   className = null,
   isAbsolute = false,
   overrideContentStructure = false,
   handleCloseClick = null,
+  isModalSource = false,
 }) => (
   <div
     className={classNames('modal', className, {
       'is-active': isActive,
       'is-absolute': isAbsolute,
+      'modal-source': isModalSource,
     })}
   >
     <div className="modal-background" onClick={handleCloseClick || popModal} />
-    <div className="modal-card">
+    <div className={classNames('modal-card', { 'modal-card-source': isModalSource })}>
       {title && (
         <header className="modal-card-head">
           <div className="modal-card-title">{title}</div>
@@ -38,6 +41,7 @@ const Modal = ({
           <CloseButton size="1.5em" title="Close" onClick={handleCloseClick || popModal} />
         </header>
       )}
+      {customHeader}
       {overrideContentStructure ? (
         children
       ) : (
