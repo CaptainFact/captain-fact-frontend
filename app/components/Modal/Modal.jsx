de76b77487952f415256bea23567d8945291b1ad
Wid-Gaël Destin <wid.destin@gmail.com>
2021-03-02 18:31:52 +0100

    Pop modal when location change

--- app/components/Modal/Modal.jsx
+++ app/components/Modal/Modal.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
-import { Link } from 'react-router'
+import { browserHistory, Link } from 'react-router'
 import classNames from 'classnames'
 
 import { popModal } from '../../state/modals/reducer'
@@ -20,36 +20,49 @@ const Modal = ({
   overrideContentStructure = false,
   handleCloseClick = null,
   isModalSource = false,
-}) => (
-  <div
-    className={classNames('modal', className, {
-      'is-active': isActive,
-      'is-absolute': isAbsolute,
-      'modal-source': isModalSource,
-    })}
-  >
-    <div className="modal-background" onClick={handleCloseClick || popModal} />
-    <div className={classNames('modal-card', { 'modal-card-source': isModalSource })}>
-      {title && (
-        <header className="modal-card-head">
-          <div className="modal-card-title">{title}</div>
-          {helpLink && (
-            <Link to={helpLink} className="help-link" target="_blank">
-              <Icon name="question-circle" size="medium" />
-            </Link>
-          )}
-          <CloseButton size="1.5em" title="Close" onClick={handleCloseClick || popModal} />
-        </header>
-      )}
-      {customHeader}
-      {overrideContentStructure ? (
-        children
-      ) : (
-        <section className="modal-card-body">{children}</section>
-      )}
-      {footer && <footer className="modal-card-foot">{footer}</footer>}
+}) => {
+  useEffect(() => {
+    const backListener = browserHistory.listen((location) => {
+      if ('POP' === location.action) {
+        popModal()
+      }
+    })
+    return () => {
+      backListener()
+    }
+  })
+
+  return (
+    <div
+      className={classNames('modal', className, {
+        'is-active': isActive,
+        'is-absolute': isAbsolute,
+        'modal-source': isModalSource,
+      })}
+    >
+      <div className="modal-background" onClick={handleCloseClick || popModal} />
+      <div className={classNames('modal-card', { 'modal-card-source': isModalSource })}>
+        {title && (
+          <header className="modal-card-head">
+            <div className="modal-card-title">{title}</div>
+            {helpLink && (
+              <Link to={helpLink} className="help-link" target="_blank">
+                <Icon name="question-circle" size="medium" />
+              </Link>
+            )}
+            <CloseButton size="1.5em" title="Close" onClick={handleCloseClick || popModal} />
+          </header>
+        )}
+        {customHeader}
+        {overrideContentStructure ? (
+          children
+        ) : (
+          <section className="modal-card-body">{children}</section>
+        )}
+        {footer && <footer className="modal-card-foot">{footer}</footer>}
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default connect(null, { popModal })(Modal)
