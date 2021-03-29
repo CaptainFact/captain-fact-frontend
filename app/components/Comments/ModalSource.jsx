ef5787227dd88abc3c26eea18121840725d614f8
Wid-Gaël Destin <wid.destin@gmail.com>
2021-03-29 19:55:25 +0200

    Added error message when preview not available

--- app/components/Comments/ModalSource.jsx
+++ app/components/Comments/ModalSource.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
-import { connect } from 'react-redux'
+import { withRouter } from 'react-router'
+import { withNamespaces, Trans } from 'react-i18next'
+import GraphQLClient from '../../API/graphql_api'
+import { isIframeAllowed } from '../../API/graphql_queries'
 import Modal from '../Modal/Modal'
-import { popModal } from '../../state/modals/reducer'
 
 import { LoadingFrame } from '../Utils/LoadingFrame'
 import ExternalLinkNewTab from '../Utils/ExternalLinkNewTab'
 
 import styled from 'styled-components'
 import { Flex, Box } from '@rebass/grid'
-import { TimesCircle } from 'styled-icons/fa-solid/TimesCircle'
-import { LinkExternal } from 'styled-icons/octicons/LinkExternal'
+import { TimesCircle } from 'styled-icons/fa-solid'
+import { LinkExternal } from 'styled-icons/octicons'
 
 const CloseButton = styled(({ size = '1em', ...props }) => (
   <TimesCircle size={size} cursor="pointer" {...props} />
@@ -39,16 +41,23 @@ const UrlBarTitle = styled(Box)`
   white-space: nowrap;
 `
 
-const IframeContainer = styled.div`
+const IframeContainer = styled(Flex)`
   background-color: white;
   height: 100%;
 `
 
+const ErrorMessage = styled.p`
+  width: 70%;
+  text-align: center;
+`
+
+@withNamespaces('videoDebate')
+@withRouter
 class ModalSource extends React.PureComponent {
-  state = { isLoading: true }
+  state = { isIframeAllowed: false, isLoading: true }
 
   renderUrlBarHeader = (url, title, website_name) => {
-    const { popModal } = this.props
+    const { t, router } = this.props
     const titleToDisplay = title === null ? website_name : title
 
     return (
@@ -59,13 +68,14 @@ class ModalSource extends React.PureComponent {
             <UrlBarTitle ml="0.2em">{titleToDisplay}</UrlBarTitle>
           </StyledExternalLinkNewTab>
         </Box>
-        <CloseButton size="1.5em" title="Close" onClick={popModal} />
+        <CloseButton size="1.5em" title={t('modalSource.close')} onClick={router.goBack} />
       </UrlBarHeader>
     )
   }
 
   render() {
-    const { source, ...otherProps } = this.props
+    const { isIframeAllowed, isLoading } = this.state
+    const { source, router, ...otherProps } = this.props
     const { url, title, website_name } = source
 
     return (
@@ -73,20 +83,31 @@ class ModalSource extends React.PureComponent {
         customHeader={this.renderUrlBarHeader(url, title, website_name)}
         overrideContentStructure
         isModalSource
+        handleCloseClick={router.goBack}
         {...otherProps}
       >
-        <IframeContainer>
-          {this.state.isLoading && <LoadingFrame />}
-          <iframe
-            sandbox=""
-            src={url}
-            style={{
-              width: '100%',
-              height: '100%',
-              display: this.state.isLoading ? 'none' : 'block',
-            }}
-            onLoad={() => this.setState({ isLoading: false })}
-          />
+        <IframeContainer justifyContent="center" alignItems="center">
+          {isLoading && <LoadingFrame />}
+          {!isLoading && !isIframeAllowed && (
+            <ErrorMessage>
+              <Trans i18nKey="modalSource.errorMessage">
+                This source is not available to preview, click here on the link above
+                <LinkExternal size="1em" /> to open it in a new tab.
+              </Trans>
+            </ErrorMessage>
+          )}
+          {isIframeAllowed && (
+            <iframe
+              sandbox=""
+              src={url}
+              style={{
+                width: '100%',
+                height: '100%',
+                display: isLoading ? 'none' : 'block',
+              }}
+              onLoad={() => this.setState({ isLoading: false })}
+            />
+          )}
         </IframeContainer>
       </Modal>
     )
@@ -94,7 +115,15 @@ class ModalSource extends React.PureComponent {
 
   componentDidMount() {
     window.location.assign('#source')
+    GraphQLClient.query({
+      query: isIframeAllowed,
+      variables: { url: this.props.source.url },
+    }).then((result) =>
+      result.data.isIframeAllowed
+        ? this.setState({ isIframeAllowed: true })
+        : this.setState({ isLoading: false })
+    )
   }
 }
 
-export default connect(null, { popModal })(ModalSource)
+export default ModalSource
