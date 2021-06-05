b9db6a6c86199d61cc495988c79862653d855b05
wdestin <wid.destin@gmail.com>
2021-06-05 11:26:20 +0200

    Added a warning message when submitting without speaker (#821)
    * feat(StatementForm): Added a warning message when submitting without speaker
    
    * Fix prettier check

--- app/lib/react_select_theme.js
+++ app/lib/react_select_theme.js
@@ -5,7 +5,24 @@ export const ReactSelectStyles = {
     ...base,
     cursor: 'text',
     ...(isFocused && {
-      'border-color': '#6ba3a7',
+      'border-color': CFTheme.colors.primary,
+      'box-shadow': '0 0 0 0.125em rgba(107, 163, 167, 0.25)',
+    }),
+  }),
+  option: (base) => ({
+    ...base,
+    cursor: 'pointer',
+  }),
+}
+
+export const ReactSelectWarningStyles = {
+  control: (base, { isFocused }) => ({
+    ...base,
+    cursor: 'text',
+    borderColor: CFTheme.colors.yellow,
+    '&:hover': { borderColor: CFTheme.colors.yellow },
+    ...(isFocused && {
+      'border-color': CFTheme.colors.yellow,
       'box-shadow': '0 0 0 0.125em rgba(107, 163, 167, 0.25)',
     }),
   }),
