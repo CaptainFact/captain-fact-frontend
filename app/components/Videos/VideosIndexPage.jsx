b3ffa273a09d2ee13ff1eac821d585fd057529d3
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2024-07-17 18:38:47 +0200

    improve videos page layout

--- app/components/Videos/VideosIndexPage.jsx
+++ app/components/Videos/VideosIndexPage.jsx
@@ -1,4 +1,3 @@
-import { capitalize } from 'lodash'
 import React from 'react'
 import { Helmet } from 'react-helmet'
 import { withNamespaces } from 'react-i18next'
@@ -7,7 +6,6 @@ import { connect } from 'react-redux'
 import { toAbsoluteURL } from '../../lib/cf_routes'
 import { changeVideosLanguageFilter, setVideosFilter } from '../../state/user_preferences/reducer'
 import Container from '../StyledUtils/Container'
-import { Icon } from '../Utils'
 import AddVideoBtn from './AddVideoBtn'
 import PaginatedVideosContainer from './PaginatedVideosContainer'
 import VideosFilterBar from './VideosFilterBar'
@@ -36,12 +34,23 @@ export default class VideosIndexPage extends React.PureComponent {
             content="Découvrez diverses vidéos sourcées et vérifiées par la communauté CaptainFact"
           />
         </Helmet>
-        <section className="header">
-          <h2 className="title is-2">
-            <Icon name="television" />
-            <span> {capitalize(t('entities.videoFactChecking'))}</span>
+        <Container
+          display="flex"
+          justifyContent="space-between"
+          alignItems="center"
+          maxWidth="1315px"
+          mx="auto"
+          px="20px"
+          pb="24px"
+          borderBottom="1px solid #f7f7f7"
+        >
+          <h2 className="is-size-3">
+            {t('menu.factChecking')}
+            <span className="mx-1"> &rsaquo; </span>
+            {t('entities.videoFactChecking')}
           </h2>
-        </section>
+          <AddVideoBtn />
+        </Container>
         <Container
           flexDirection={['column-reverse', 'row']}
           flexWrap="wrap"
@@ -53,7 +62,6 @@ export default class VideosIndexPage extends React.PureComponent {
           px="20px"
           py="15px"
           mx="auto"
-          mt={['30px', '80px']}
           mb="32px"
           boxShadow="rgba(0, 0, 0, 0.1) 0px 10px 10px -10px"
           borderBottom="1px solid #f2f2f2"
@@ -70,7 +78,6 @@ export default class VideosIndexPage extends React.PureComponent {
             language={languageFilter}
             source={videosFilter}
           />
-          <AddVideoBtn />
         </Container>
         <PaginatedVideosContainer
           baseURL={this.props.location.pathname}
