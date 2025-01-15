327e675bc61f30c0fcde2fdf75b16866bb44f8fe
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2025-01-15 16:09:03 +0100

    deps: Update react to 18

--- app/index.jsx
+++ app/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 
 import App from './App'
 
 // Inject React app in DOM
-ReactDOM.render(<App />, document.getElementById('app'))
+const container = document.getElementById('app')
+const root = createRoot(container) // createRoot(container!) if you use TypeScript
+root.render(<App />)
